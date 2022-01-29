698d407c0076abc971ea2b3a47974f1113672a51
Rushmerk <57403682+pedroperegrinaa@users.noreply.github.com>
2022-01-29 14:41:27 -0300

    PARTICLES ADICIONADO

---
+++ particles.js
@@ -0,0 +1,9 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+
+
+console.log('aa');
+
+particlesJS.load('particles-js', './assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
