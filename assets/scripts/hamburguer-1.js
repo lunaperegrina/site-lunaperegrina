750b5dfdf27a215465af73f586bf4ae40288ff2c
pedroperegrinaa <57403682+pedroperegrinaa@users.noreply.github.com>
2022-06-28 00:06:44 -0300

    navbar ok

--- assets/scripts/hamburguer-1.js
+++ assets/scripts/hamburguer-1.js
@@ -1,11 +1,14 @@
-const bntMobile = document.getElementById('bnt-mobile');
+const btnMobile = document.getElementById('btn-mobile');
 console.log('aaa')
+console.log(btnMobile);
 
-function toggleMenu(){
+function toggleMenu() {
     const nav = document.getElementById('nav');
+    console.log('clocou');
     nav.classList.toggle('active');
 }
 
-if (bntMobile) {
-    bntMobile.addEventListener('click', toggleMenu);
+if (btnMobile) {
+    console.log('bbb');
+    btnMobile.addEventListener('click', toggleMenu);
 }
